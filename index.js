488e38c64f80b1a608eab006b03bacafe1d84573
manthoshkumar.t <manthoshkumar.t@zohocorp.com>
2016-12-18 17:51:10 +0530

    Removing oauth, splitwise and just using horseman which just works :)

--- index.js
+++ index.js
@@ -1,167 +1,112 @@
 'use strict'
 const http = require('http');
 const express = require('express');
-// const OAuth = require('../lib/oauth.js').OAuth;
-const AuthAPI = require('splitwise-node');
 const Horseman = require('node-horseman');
 
 let app = express();
 
-// let oa = new OAuth(
-// 		'https://secure.splitwise.com/api/v3.0/get_request_token',
-// 		'https://secure.splitwise.com/api/v3.0/get_access_token',
-// 		'XtcFcRpKBdw3Xo593e2eAQqIVv01tfrNgJcRNxcj',
-// 		'yBy1YRKY3MHfa9kaELSyqszNwYSvd8tmU94XmtmT',
-// 		'1.0',
-// 		'https://test-splitwise.herokuapp.com/',
-// 		'HMAC-SHA1'
-// 	);
-
 let horseman = new Horseman();
-var loginAction = function(actionCB) {
+horseman.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0');
+var loginAction = function() {
+	console.log("Logging in...");
 	horseman
-	  .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
 	  .open('https://www.splitwise.com')
 	  .click('.btn.btn-mint')
 	  .type('#user_session_email', 'manthosh@gmail.com')
 	  .type('#user_session_password', 'tvrM1991')
 	  .click('input[type=submit]')
 	  .wait(10000)
-	  .log();
-	  // .do(actionCB)
-	  // .close();
+	  .text('.dropdown-toggle')
+	  .then((text) => {
+	            console.log(text);
+	   });
 }
 
-var userOAuthToken, userOAuthTokenSecret;
-var authApi;
-
-var init = function() {
-	try {
-		// console.log('Init');
-		// authApi = new AuthAPI(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
-		// var userAuthUrl = authApi.getOAuthRequestToken()
-		// 						.then(({ token, secret }) => {
-
-	 //    								[userOAuthToken, userOAuthTokenSecret] = [token, secret];
-	 //    								return authApi.getUserAuthorisationUrl(token);
-		// 							});
-		// userAuthUrl.then((uri) => {
-		// 	loginAction(function() {
-	 //      		horseman.openTab(uri)
-	 //      				.click('input[value=Authorize]')
-	 //      				.wait(10000);
-		// 	});
-		// }); 
-		loginAction();
-	}
-	catch(err) {
-		setTimeout(init,2000);
-	}						
+var getFriend = function(id) {
+	return new Promise((fulfill, reject) => {
+		horseman.open('https://secure.splitwise.com/api/v3.0/get_friend/'+id)
+				.waitForSelector('pre')
+				.html('pre')
+				.then(function(response) {
+					// console.log(response);
+					// return JSON.parse(response);
+					fulfill(JSON.parse(response));
+				});
+	});
 }
 
-init();
+var addExpense = function(firstName, amount) {
+	console.log("Adding Expense...");
+	horseman.open('https://secure.splitwise.com/#/all')
+			.waitForSelector('a[data-original-title=Quick-add]')
+			.click('a[data-original-title=Quick-add]')
+			.type('#quickadd',firstName+' owes me '+amount)
+			.click('button[data-loading-text=Add]')
+			.wait(10000);
+}
+
+var isRunning = false;
+var previousBalance = 0;
+var fetchBalance = function() {
+		isRunning = true;
+		var response = getFriend(2635429);
+		// var response = getFriend(238606);
+		response.then((friend) => {
+			// console.log("Manthosh");
+			// console.log(friend);
+
+			if(friend) {
+				if(friend.error == "Invalid API Request: you are not logged in") {
+					loginAction();
+					// fetchBalance();
+				}
+				else {
+					var firstName = friend.friend.first_name;
+					var balance = parseFloat(friend.friend.balance[0].amount);
+
+					console.log(firstName+"=>"+balance);
+
+					if(balance == 0) {
+						addExpense(firstName, previousBalance);
+					}
+					previousBalance = balance;
+				}
+			}
+
+		});
+}
+
+fetchBalance();
+var intervalTimeoutObj = setInterval(fetchBalance, 30000);
 
 app.get('/', (req, res) => {
     return res.end("<h1>Hello World</h1>");
 })
 
-
-
-app.get('/requestAccess', (req, res) => {
-	init();
-	res.end("Done!!");   							
-})
-
-app.get('/callback', (req, res) => {
-	console.log("verifier");
-	console.log(req.query.oauth_verifier);
-	authApi.getOAuthRequestToken()
-			.then(({ token, secret }) => {
-				[userOAuthToken, userOAuthTokenSecret] = [token, secret];
-				console.log("secrets");
-				console.log(userOAuthToken);
-				console.log(userOAuthTokenSecret);
-				return res.end("<h1>Magizhchi</h1>");
-			});				
-})
-
-var intervalTimeoutObj, splitwiseApi = null;
 app.get('/start', (req, res) => {
-
-	// horseman.do(function() {
-	// loginAction(function() {	
-	// 	if(splitwiseApi == null) {
-	// 		splitwiseApi = authApi.getSplitwiseApi(userOAuthToken, userOAuthTokenSecret);
-	// 	}
-	// 	console.log(splitwiseApi);
-
-	// 	splitwiseApi.getFriends().then((friendRes) => {
-	// 								console.log("Manthosh");
-	// 								console.log(friendRes);
-	// 							});
-
-	// 	splitwiseApi.__auth.get('https://secure.splitwise.com/api/v3.0/get_friend/2635429', splitwiseApi.oAuthToken, splitwiseApi.oAuthTokenSecret, function(response) {
-	// 								console.log(response);
-	// 								//return res.end("<h1>"+JSON.stringify(response)+"</h1>");
-	// 							});
-	// });	
-
-	// if(splitwiseApi == null) {
-	// 	splitwiseApi = authApi.getSplitwiseApi(userOAuthToken, userOAuthTokenSecret);
-	// }
-
-	// horseman.open('https://www.splitwise.com')
-	// 		.wait(10000)
-	// 		.evaluate(function(splitwiseApi) {
-				
-	// 				console.log(splitwiseApi);
-
-	// 				splitwiseApi.getFriends().then((friendRes) => {
-	// 											console.log("Manthosh");
-	// 											console.log(friendRes);
-	// 										});
-
-	// 				splitwiseApi.__auth.get('https://secure.splitwise.com/api/v3.0/get_friend/2635429', splitwiseApi.oAuthToken, splitwiseApi.oAuthTokenSecret, function(response) {
-	// 											console.log(response);
-	// 											//return res.end("<h1>"+JSON.stringify(response)+"</h1>");
-	// 										});
-	// 		}, splitwiseApi)
-	// 		 .close();
-
-	horseman.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-			.open('https://secure.splitwise.com/api/v3.0/get_friend/2635429')
-			.waitForSelector('pre')
-			.html('pre')
-			.then(function(response) {
-				console.log(response);
-				return res.end("<h1>"+JSON.stringify(response)+"</h1>")
-			})
-			.close();
-
-    // return res.end("<h1>Started</h1>");	
+	if(isRunning) {
+		return res.end("<h1>Alerady Running</h1>");	
+	}
+	else {
+		fetchBalance();
+		intervalTimeoutObj = setInterval(fetchBalance, 30000);
+		return res.end("<h1>Started</h1>");	
+	}
 })
 
 app.get('/stop', (req, res) => {
-	clearInterval(intervalTimeoutObj);
-    return res.end("<h1>Stopped</h1>");
+	if(isRunning) {
+		clearInterval(intervalTimeoutObj);
+		isRunning = false;
+	    return res.end("<h1>Stopped</h1>");
+	}
+	else {
+		return res.end("<h1>Already Stopped</h1>");	
+	}
 })
 
 app.get('/status', (req, res) => {
-	if(splitwiseApi == null) {
-		splitwiseApi = authApi.getSplitwiseApi(userOAuthToken, userOAuthTokenSecret);
-	}
-	splitwiseApi.isServiceOk().then((status) => {
-		return res.end("<h1>"+status+"</h1>");	
-	});
-    
+	
 })
-
-// var getRequestToken = function(req, res) {
-// 	oa.getOAuthRequestToken(function (error, oAuthToken, oAuthTokenSecret, results) {
-
-// 	});
-// }
-
-
 
 http.createServer(app).listen(process.env.PORT || 3000)
